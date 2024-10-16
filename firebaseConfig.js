eba6463f5e9e49f7bb03e04ea728cd78d71caa92
Fo-Sho <141362811+Fo-Sho@users.noreply.github.com>
2024-10-17 00:20:32 +0200

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -5,15 +5,7 @@ import { getDatabase } from 'firebase/database';
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDoP-JVzlindZ-uwHsw0pWX-yEVS9fk40c",
-  authDomain: "inventory-management-31d31.firebaseapp.com",
-  databaseURL: "https://inventory-management-31d31-default-rtdb.europe-west1.firebasedatabase.app/",
-  projectId: "inventory-management-31d31",
-  storageBucket: "inventory-management-31d31.appspot.com",
-  messagingSenderId: "93968563841",
-  appId: "1:93968563841:web:9a3d30c734bd24dd04d780",
-  measurementId: "G-5ZZWV1GYZN"
-};
+ /7 removed
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
